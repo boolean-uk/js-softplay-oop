961281e73c812c0bd01de4b44eb8133403a4f9c2
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-03-11 16:41:17 +0100

    completed extensions

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,49 +1,41 @@
 // TODO: Write your class in this file
 
-// You need to write a program that maintains counts of both the number of children and adults inside a soft play center, using a class inside the src/soft-play-oop.js file. Your class should have the following methods:
-// SoftPlay#occupancy(), SoftPlay#enter(numAdults, numChildren), SoftPlay#leave(numAdults, numChildren)
-// SoftPlay#occupancy() This function should return an object with two keys - adults should contain the number of adults currently inside the soft play center and children the number of children.
-// This function is used to register adults and children entering the soft play center. The function should check for the following conditions:
-// Every child entering the soft play center is accompanied by at least 1 adult.
-// If any of these checks fail, the function should return false. Otherwise, the function should return true and numAdults and numChildren should be added to the current totals.
-// This function is used to register adults and children leaving the soft play center. The function should check for the following conditions:
-// A child is not attempting to leave without an adult
-// The number of adults and children left inside the center will not cause there to be more children than adults (there must be at least 1 adult for 1 child inside the center at all times)
-// Every child leaving the soft play center is accompanied by at least 1 adult.
-// The number of adults and children attempting to leave is not greater than the number currently inside the center
-
+// main class
 class SoftPlay {
+  // constructor
   constructor() {
     this.adults = 0;
     this.children = 0;
   }
+  // class to store arrays
   occupancy() {
     return {
       adults: this.adults,
       children: this.children
     }
   }
-  enter(numAdults, numChildren) {
+  // class to describe enter
+  enter(numAdults, numChildren, maxOccupancy, childrenPerAdult) {
     // under what conditions
-    if (numAdults >= numChildren){
+    // number of adults entering the play center should be equal or more the number of children
+    if (numAdults >= numChildren / childrenPerAdult){
       this.adults += numAdults;
       this.children += numChildren;
-      console.log(this.occupancy())
-    return true} 
+      this.adults + this.children <= maxOccupancy
+      return true} 
     else{
       return false
     }
   }
+  // class to describe leave
 
-  leave(numAdults, numChildren) {
+  leave(numAdults, numChildren, childrenPerAdult) {
     // under what conditions
+    // number of adults in play area should never be below number of children
+    // number of adults or children leaving should not be more than the number of adults or children at the play center
     if (numAdults !== 0){
-      if(this.adults - numAdults < this.children - numChildren) {
-        return false
-      }
-      if(this.adults < numAdults || this.children < numChildren) {
-        return false
-      }
+      if(this.adults < numAdults || this.children < numChildren) {return false}
+      if((this.adults - numAdults) * childrenPerAdult < this.children - numChildren) {return false}
       this.adults -= numAdults;
       this.children -= numChildren;
       return true
