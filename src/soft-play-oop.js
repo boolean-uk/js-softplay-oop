d0a29e9f0f1e382c0ecc512b6b07ce3c9cf2da2a
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-11 12:58:09 +0100

    FT: SoftPlay

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,9 @@
+const SoftPlay =  require('./SoftPlay.js')
 // TODO: Write your class in this file
+
+
 
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
+  SoftPlay: SoftPlay
 }
