1db25a24e5101a905f9a2d3a09688b19b61b33da
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-11 18:29:50 +0100

    added the extension

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,43 +1,49 @@
 // TODO: Write your class in this file
 class SoftPlay {
-  constructor() {
+  constructor (maxOccupancy) {
     this.numAdults = 0;
     this.numChildren = 0;
+    this.maxOccupancy = maxOccupancy || false
   }
 
-  enter(adults, children) {
-    if (adults < children) {
-      return false;
-    } else {
-      this.numAdults += adults;
-      this.numChildren += children;
-      return true;
-    }
-  }
-
-  leave(adults, children) {
-    if (adults <= 0) {
-      return false;
-    } else if (adults > this.numAdults || children > this.numChildren) {
-      return false;
-    } else if (this.numAdults - adults < this.numChildren - children) {
-      return false;
-    } else {
-      this.numAdults -= adults;
-      this.numChildren -= children;
-      return true;
-    }
-  }
-
-  occupancy() {
+  occupancy () {
     return {
       adults: this.numAdults,
       children: this.numChildren,
     };
   }
+  
+  capacity (adults, children) {
+      if (this.maxOccupancy === false) return true;
+  
+      return (
+        this.numChildren + children + this.numAdults + adults <= this.maxOccupancy
+      );
+  }
+
+  enter (adults, children,) {
+    if (adults < children) return false;
+    if (!this.capacity(adults, children)) return false;
+    
+      this.numAdults += adults;
+      this.numChildren += children;
+      return true;
+    }
+  
+
+  leave (adults, children) {
+    if (adults <= 0) return false;
+    if (adults > this.numAdults || children > this.numChildren) return false;
+    if (this.numAdults - adults < this.numChildren - children) return false;
+      
+    this.numAdults -= adults;
+    this.numChildren -= children;
+    return true;
+  }
 }
+
 
 // TODO: Change undefined to the name of your class
 module.exports = {
   SoftPlay: SoftPlay,
-};
+}
