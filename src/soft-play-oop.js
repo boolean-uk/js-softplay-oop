bf7ede9364ddbfee0931c09c4d2a60329767c1cd
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-11 12:07:14 +0100

    passed the test for soft-play-oop.js

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,45 @@
 // TODO: Write your class in this file
+class SoftPlay {
+  constructor() {
+    this.numAdults = 0;
+    this.numChildren = 0;
+  }
+
+  enter(adults, children) {
+    if (adults >= 1) {
+      this.numAdults += adults;
+      this.numChildren += children;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  leave(adults, children) {
+    if (
+      adults >= children &&
+      adults > 0 &&
+      this.numAdults > this.numChildren &&
+      this.numAdults >= adults &&
+      this.numChildren >= children
+    ) {
+      this.numAdults -= adults;
+      this.numChildren -= children;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren,
+    };
+  }
+}
 
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
-}
+  SoftPlay: SoftPlay,
+};
