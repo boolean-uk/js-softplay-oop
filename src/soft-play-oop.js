b7f85ca662c69c89017565e6c8ee88e5f65d6264
Dan <dannorthcott8@gmail.com>
2022-03-11 15:08:06 +0000

    softplay passed tests

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,37 @@
 // TODO: Write your class in this file
 
+class SoftPlay {
+  constructor() {
+    this.adults = 0;
+    this.children = 0;
+  }
+  occupancy() {
+    return {
+      adults: this.adults,
+      children: this.children,
+    };
+  }
+  enter(numAdults, numChildren) {
+    // guard clause
+    if (numAdults < numChildren) return false;
+
+    this.adults += numAdults;
+    this.children += numChildren;
+    this.occupancy();
+    return true;
+  }
+  leave(numAdults, numChildren) {
+    if (numAdults < numChildren) return false;
+    if (this.adults - numAdults < this.children - numChildren) return false;
+    if (numAdults > this.adults || numChildren > this.children) return false;
+
+    this.adults -= numAdults;
+    this.children -= numChildren;
+    return true;
+  }
+}
+
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
-}
+  SoftPlay: SoftPlay,
+};
